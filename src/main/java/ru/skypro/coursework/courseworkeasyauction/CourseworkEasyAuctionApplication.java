ff933ea927a2d2f47c22c795aac0430152997c5f
vyacheslavbagin@gmail.com <vyacheslavbagin@gmail.com>
2023-07-15 17:14:12 +0300

    Мастер ветка

---
+++ src/main/java/ru/skypro/coursework/courseworkeasyauction/CourseworkEasyAuctionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.coursework.courseworkeasyauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkEasyAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkEasyAuctionApplication.class, args);
+    }
+
+}
